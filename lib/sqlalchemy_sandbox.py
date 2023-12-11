5669f96ebbdf6423d5d2045b6cd8490c0a4c4673
james <jimmindungu3@gmail.com>
2023-12-11 16:34:24 +0300

    create student db

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import Column, Integer, String, create_engine
+# Import necessary libraries for SQLAlchemy and database connections
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
 
+# Create an engine object to connect to the SQLite database named "students.db"
+engine = create_engine('sqlite:///students.db')
+
+# Define a base class for declarative model creation
 Base = declarative_base()
 
-class Student(Base):
-    pass
+# Create all tables defined by SQLAlchemy models in the "students.db" database
+Base.metadata.create_all(engine)
 
+# Define a class named "Student" that inherits from the base class
+class Student(Base):
+    # Specify the name of the table in the database represented by the "Student" model
+    __tablename__ = 'students'
+
+    # Define an integer column named "id" as the primary key of the table
+    id = Column(Integer(), primary_key=True)
+
+    # Define a string column named "name" for storing student names
+    name = Column(String())
+
+# The following block is only executed when the script is run directly and not imported as a module
 if __name__ == '__main__':
     pass
+
+# Add code here to perform operations like adding new students, retrieving student information, or updating existing students
